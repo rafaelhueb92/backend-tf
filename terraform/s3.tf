9b0fd505c4bc03fba04878b8f702fbd6d5f50c45
Rafael Hueb <rafaelhueb@Rafaels-MacBook-Air.local>
2025-02-06 19:16:51 -0300

    resolve var problem

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "this" {
-  bucket = var.bucket_name
+  bucket = "${var.bucket_name}-${data.aws_caller_identity.current.account_id}"
 
   lifecycle {
     prevent_destroy = true  
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,4 @@
 variable "bucket_name" {
     type = string
-    default = "backend-tf-${data.aws_caller_identity.current.account_id}"
+    default = "backend-tf"
 }
